4e76660a1072aca483b2b50432c147402555b679
Elysia Gabe <elysiagabe@gmail.com>
2020-04-08 15:18:17 -0700

    Creates react app and sets up very basic component structure

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Users from './components/Users';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the homepage</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserProfile from './UserProfile';
+
+const Users = () => {
+    return (
+        <div>
+            <h2>This is where all the users will be listed.</h2>
+            <UserProfile />
+        </div>
+    )
+}
+
+export default Users;
